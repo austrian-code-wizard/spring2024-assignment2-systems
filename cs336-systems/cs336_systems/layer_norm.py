46d833ea5d58bc064d333e6dd9189dbb5e6f3178
Moritz Stephan <moritz.stephan@stanford.edu>
2024-04-26 16:44:11 -0700

    adding norm tests

---
+++ cs336-systems/cs336_systems/layer_norm.py
@@ -0,0 +1,42 @@
+from cs336_basics.model import RMSNorm
+import torch
+import time
+
+
+ITERS = 1000
+ROWS = 50000
+COLS = [1024, 2048, 4096, 8192]
+
+
+def main():
+    for col in COLS:
+        data = torch.randn(ROWS, col).to("cuda")
+
+        layer_norm = torch.nn.LayerNorm(col).to("cuda")
+        rms_norm = RMSNorm(col).to("cuda")
+
+        # warmup
+        for _ in range(10):
+            layer_norm(data)
+            torch.cuda.synchronize()
+        start = time.time()
+        for _ in range(ITERS):
+            layer_norm(data)
+            torch.cuda.synchronize()
+        end = time.time()
+        print(f"LayerNorm: {col} cols took {end - start} seconds")
+
+        # warmup
+        for _ in range(10):
+            rms_norm(data)
+            torch.cuda.synchronize()
+        start = time.time()
+        for _ in range(ITERS):
+            rms_norm(data)
+            torch.cuda.synchronize()
+        end = time.time()
+        print(f"RMSNorm: {col} cols took {end - start} seconds")
+
+
+if __name__ == "__main__":
+    main()
