8ef1afc006874372d9a5d1edcb358d15ba0ab60e
Moritz Stephan <moritz.stephan@stanford.edu>
2024-04-28 18:44:08 -0700

    adding triton

--- cs336-systems/cs336_systems/rms.py
+++ cs336-systems/cs336_systems/rms.py
@@ -27,7 +27,7 @@ def rmsnorm_grad_x(x, weight, grad_out, eps):
     x = x.view(-1, x.shape[-1])
     grad_out = grad_out.view(-1, grad_out.shape[-1])
     norm = torch.sqrt(torch.mean(x ** 2, dim=-1, keepdim=True) + eps)
-    grad_x = (grad_out - x * torch.sum(grad_out * x, dim=-1, keepdim=True) / torch.mean(x ** 2, dim=-1, keepdim=True)) * weight / norm
+    grad_x = grad_out * weight / norm - 0.5 * x * torch.sum(grad_out * x, dim=-1, keepdim=True) * weight / norm ** 3
     return grad_x.view(*x_shape)
 
 class RMSNorm(torch.autograd.Function):
