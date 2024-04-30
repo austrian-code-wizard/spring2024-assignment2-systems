2a4611654e808622e6fd64cb9d23f31348b87719
Moritz Stephan <moritz.stephan@stanford.edu>
2024-04-29 18:30:43 -0700

    adding triton

--- cs336-systems/cs336_systems/rms.py
+++ cs336-systems/cs336_systems/rms.py
@@ -34,7 +34,7 @@ def rmsnorm_grad_x(x, weight, grad_out, eps):
     norm = torch.sqrt(torch.mean(x ** 2, dim=-1, keepdim=True) + eps)
 
     numerator = x * grad_out * weight.view(1, -1) / (x_shape[-1] * norm ** 3)
-    second_term = torch.bmm(numerator.unsqueeze(-2), x.unsqueeze(-1)).squeeze(-1).sum(dim=-1)
+    second_term = torch.bmm(numerator.unsqueeze(-2), x.unsqueeze(-1)).squeeze(-1).sum(dim=1)
 
     first_term = grad_out * weight.view(1, -1) / norm
     return (first_term - x * second_term.view(-1, 1)).view(*x_shape)
